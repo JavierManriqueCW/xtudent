827ab85d560915697a5da546d22295356e1fe7d0
Javier Manrique <>
2024-11-02 13:18:46 +0100

    small intentional error for screenshot testing demo

--- exams_feature/presentation/src/main/java/com/jmp/examsfeature/presentation/list/screens/EmptyExamsScreen.kt
+++ exams_feature/presentation/src/main/java/com/jmp/examsfeature/presentation/list/screens/EmptyExamsScreen.kt
@@ -59,7 +59,7 @@ fun EmptyExamsScreen(modifier: Modifier = Modifier) {
                 color = Color.LightGray,
                 fontWeight = FontWeight.ExtraLight,
                 lineHeight = 60.sp,
-                text = "No exams imported",
+                text = "No exams importedddd",
                 fontSize = 24.sp,
             )
         }
